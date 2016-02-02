1d6db971e2162909b117cf2b6230f804e2970717
metavige <metavige@gmail.com>
2016-02-02 16:43:36 +0800

    修改 indexer config
    原本要使用另外一個 logstash 來做 filter
    不過因為協定關係，目前沒有試成功
    現在先改採 indexer 的設定檔，做成多個
    根據 input, filter, output 不同的樣子分成不同的 conf
    之後再考慮增加設定的作法

---
+++ logspout-tcp-collector/modules.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+  _ "github.com/gliderlabs/logspout/httpstream"
+  _ "github.com/gliderlabs/logspout/routesapi"
+  _ "github.com/gliderlabs/logspout/adapters/syslog"
+  _ "github.com/gliderlabs/logspout/transports/tcp"
+)
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gosu elasticsearch elasticsearch &
-logstash agent -f /elk/logstash/config/logstash.conf &
-kibana
